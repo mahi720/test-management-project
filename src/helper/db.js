f572a5b84f33fa447ac2c65cc111bdfce5e900a6
Sonu Kumar <121798722+mahi720@users.noreply.github.com>
2024-08-21 16:57:39 +0530

    Delete src directory

--- src/helper/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require('dotenv').config();
-// const SERVER = ;
-const connection = mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("connection established"))
-  .catch((err) => console.log(err));
-
-module.exports = { connection };
